33cd58af4d6b2aa4fef050deb8dc7f486a3f096d
friedger <mail@friedger.de>
2023-02-19 16:52:54 +0100

    add tests for donate.clar

--- packages/friedger-pool-extras/contracts/donate.clar
+++ packages/friedger-pool-extras/contracts/donate.clar
@@ -11,29 +11,36 @@
 
 ;; private functions
 ;;
-(define-private (add-percentage (rewards {recipient: principal, percentage: uint}) (sum uint))
+(define-private (add-percentage (rewards {
+                                    recipient: (optional principal),
+                                    recipient-burnchain: (optional {version: (buff 1), hashbytes: (buff 32)}),
+                                    percentage: uint }) 
+                                (sum uint))
     (+ sum (get percentage rewards)))
 
 ;; public functions
 ;;
 ;; mapping between stacker and recipient of stacker's rewards
 ;; (define-map payout-map principal (list 10 {recipient: principal, percentage: uint}))
-(define-map payout-map principal 
+(define-map payout-map principal
   (list 10 {
-    recipient: (optional principal), 
-    recipient-burnchain: (optional {version: (buff 1), hashbytes: (buff 32)}), 
+    recipient: (optional principal),
+    recipient-burnchain: (optional {version: (buff 1), hashbytes: (buff 32)}),
     percentage: uint }))
 
 (define-read-only (get-payout-recipients (stacker principal))
     (map-get? payout-map stacker))
-    
-;; set receiver of the stacker's rewards
-(define-public (set-payout-recipients (rewards-distribution (list 10 {recipient: principal, percentage: uint})))
-    (begin 
+   
+;; set recipients of the stacker's rewards
+(define-public (set-payout-recipients (rewards-distribution (list 10 {
+        recipient: (optional principal),
+        recipient-burnchain: (optional {version: (buff 1), hashbytes: (buff 32)}),
+        percentage: uint })))
+    (begin
         (asserts! (<= (fold add-percentage rewards-distribution u0) u100) err-invalid-recipients)
         (ok (map-set payout-map tx-sender rewards-distribution))))
 
-;; remove receiver of the stacker's rewards. Rewards will be sent to stacker
-(define-public (delete-payout-recipient (recipient principal))
+;; Remove recipients of the stacker's rewards. All rewards will be sent to stacker
+(define-public (delete-payout-recipients)
     (ok (map-delete payout-map tx-sender)))
 
--- packages/friedger-pool-extras/tests/booster_test.ts
+++ packages/friedger-pool-extras/tests/booster_test.ts
@@ -1,26 +1,187 @@
-
-import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarinet@v1.3.0/index.ts';
-import { assertEquals } from 'https://deno.land/std@0.170.0/testing/asserts.ts';
+import {
+  Clarinet,
+  Tx,
+  Chain,
+  Account,
+  types,
+  assertEquals,
+} from "../../common/tests/deps.ts";
+("../deps.ts");
 
 Clarinet.test({
-    name: "Ensure that <...>",
-    async fn(chain: Chain, accounts: Map<string, Account>) {
-        let block = chain.mineBlock([
-            /*
-             * Add transactions with:
-             * Tx.contractCall(...)
-            */
-        ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 2);
+  name: "Ensure that user can manage recipients",
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    const deployer = accounts.get("deployer")!;
+    const wallet_1 = accounts.get("wallet_1")!;
+    let block = chain.mineBlock([
+      Tx.contractCall(
+        "booster",
+        "set-payout-recipients",
+        [
+          types.list([
+            types.tuple({
+              recipient: types.some(types.principal(wallet_1.address)),
+              "recipient-burnchain": types.none(),
+              percentage: types.uint(50),
+            }),
+          ]),
+        ],
+        deployer.address
+      ),
+    ]);
 
-        block = chain.mineBlock([
-            /*
-             * Add transactions with:
-             * Tx.contractCall(...)
-            */
-        ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 3);
-    },
+    block.receipts[0].result.expectOk().expectBool(true);
+
+    let response = chain.callReadOnlyFn(
+      "booster",
+      "get-payout-recipients",
+      [types.principal(deployer.address)],
+      deployer.address
+    );
+
+    const recipients = response.result.expectSome().expectList();
+    recipients[0].expectTuple()["percentage"].expectUint(50);
+    recipients[0]
+      .expectTuple()
+      ["recipient"].expectSome()
+      .expectPrincipal(wallet_1.address);
+    recipients[0].expectTuple()["recipient-burnchain"].expectNone();
+
+    block = chain.mineBlock([
+      Tx.contractCall(
+        "booster",
+        "delete-payout-recipients",
+        [],
+        deployer.address
+      ),
+    ]);
+
+    block.receipts[0].result.expectOk().expectBool(true);
+
+    response = chain.callReadOnlyFn(
+      "booster",
+      "get-payout-recipients",
+      [types.principal(deployer.address)],
+      deployer.address
+    );
+
+    response.result.expectNone();
+  },
+});
+
+Clarinet.test({
+  name: "Ensure that user can not distribute more than 100 percent",
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    const deployer = accounts.get("deployer")!;
+    const wallet_1 = accounts.get("wallet_1")!;
+    const wallet_2 = accounts.get("wallet_2")!;
+    let block = chain.mineBlock([
+      Tx.contractCall(
+        "booster",
+        "set-payout-recipients",
+        [
+          types.list([
+            types.tuple({
+              recipient: types.some(types.principal(wallet_1.address)),
+              "recipient-burnchain": types.none(),
+              percentage: types.uint(50),
+            }),
+            types.tuple({
+              recipient: types.some(types.principal(wallet_2.address)),
+              "recipient-burnchain": types.none(),
+              percentage: types.uint(70),
+            }),
+          ]),
+        ],
+        deployer.address
+      ),
+    ]);
+
+    block.receipts[0].result.expectErr().expectUint(500);
+  },
+});
+
+Clarinet.test({
+  name: "Ensure that user can not distribute more than 100 percent across both chains",
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    const deployer = accounts.get("deployer")!;
+    const wallet_1 = accounts.get("wallet_1")!;
+    const wallet_2 = accounts.get("wallet_2")!;
+    let block = chain.mineBlock([
+      Tx.contractCall(
+        "booster",
+        "set-payout-recipients",
+        [
+          types.list([
+            types.tuple({
+              recipient: types.some(types.principal(wallet_1.address)),
+              "recipient-burnchain": types.none(),
+              percentage: types.uint(70),
+            }),
+            types.tuple({
+              recipient: types.none(),
+              "recipient-burnchain": types.some(
+                types.tuple({
+                  version: "0x05",
+                  hashbytes: "0x01020304",
+                })
+              ),
+              percentage: types.uint(70),
+            }),
+          ]),
+        ],
+        deployer.address
+      ),
+    ]);
+
+    block.receipts[0].result.expectErr().expectUint(500);
+  },
+});
+
+Clarinet.test({
+  name: "Ensure that user can set recipients on burnchain",
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    const deployer = accounts.get("deployer")!;
+    const wallet_1 = accounts.get("wallet_1")!;
+    let block = chain.mineBlock([
+      Tx.contractCall(
+        "booster",
+        "set-payout-recipients",
+        [
+          types.list([
+            types.tuple({
+              recipient: types.none(),
+              "recipient-burnchain": types.some(
+                types.tuple({
+                  version: "0x05",
+                  hashbytes: "0x01020304",
+                })
+              ),
+              percentage: types.uint(50),
+            }),
+          ]),
+        ],
+        deployer.address
+      ),
+    ]);
+
+    block.receipts[0].result.expectOk().expectBool(true);
+
+    let response = chain.callReadOnlyFn(
+      "booster",
+      "get-payout-recipients",
+      [types.principal(deployer.address)],
+      deployer.address
+    );
+
+    const recipients = response.result.expectSome().expectList();
+    recipients[0].expectTuple()["percentage"].expectUint(50);
+    recipients[0].expectTuple()["recipient"].expectNone();
+    const burnchainRecipient = recipients[0]
+      .expectTuple()
+      ["recipient-burnchain"].expectSome()
+      .expectTuple();
+    burnchainRecipient["version"].expectBuff(new Uint8Array([5]));
+    burnchainRecipient["hashbytes"].expectBuff(new Uint8Array([1, 2, 3, 4]));
+  },
 });
